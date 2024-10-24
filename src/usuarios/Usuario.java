773dd1470204b63be2b721bc02cbcec082d0eda4
Leslye <l24120363@morelia.tecnm.mx>
2024-10-23 22:12:07 -0600

    Tarea 5

---
+++ src/usuarios/Usuario.java
@@ -0,0 +1,101 @@
+package usuarios;
+
+import usuarios.utils.Rol;
+
+import java.time.LocalDate;
+
+public class Usuario {
+    public String id;
+    public String nombre;
+    public String apellidos;
+    public LocalDate fechaNacimiento;
+    public String telefono;
+    public Rol rol;//enum (es una clase que almacena constantes, valores que nunca van a cambiar
+    private String contrasenia;
+    public String correoElectronico;
+
+    public Usuario(String id, String nombre, String apellidos, LocalDate fechaNacimiento, String telefono, String contrasenia,Rol rol, String correoElectronico) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.fechaNacimiento = fechaNacimiento;
+        this.telefono = telefono;
+        this.contrasenia = contrasenia;
+        this.rol = rol;
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getContrasenia() {//el protected sirve para que solo la clase padre y las clases hijas tengas acceso
+        return contrasenia;
+    }
+
+    public Rol getRol() {
+
+        return rol;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public String mostrarInformacion(){
+        String nombreCompleto = this.nombre+ ""+ this.apellidos;
+        String datos = String.format(
+                "\nId: %s,nombre completo: %s, fecha d nacimiento:%s, telefono: %s, correo: s%"
+                ,this.id,nombreCompleto, this.fechaNacimiento,this.telefono, this.correoElectronico
+        );
+        return datos;
+    }
+}
